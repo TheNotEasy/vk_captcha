c3985b95c0dc6d3d9b6bca4e23932671be06ac15
WireguardVpnRender <167197561+WireguardVpnRender@users.noreply.github.com>
2024-06-30 15:01:15 +0900

    Update setup.py

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ with open("./requirements.txt") as req_file:
 setup(
   name='vk_captcha',
   packages=['vk_captcha'],
-  version='2.0.2',
+  version='2.0.3',
   license='MIT',
   author='IMCorp',
 #  description='Library to solve vk captcha async/sync.\nFree.\nHigh speed.',
